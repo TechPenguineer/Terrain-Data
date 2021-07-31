f3dff3995e557163a0c0cdf7ad5f25865480ce96
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-31 11:58:34 -0400

    add console dock

--- src/display.py
+++ src/display.py
@@ -6,14 +6,15 @@ import numpy as np
 from pyqtgraph.dockarea import *
 
 version = "1.0.0"
-win_title = "Terrain Data - {}", format(version)
 # CREATE APP WINDOW
 app = pg.mkQApp("")
 win = QtGui.QMainWindow()
 area = DockArea()
 win.setCentralWidget(area)
 win.resize(1000,500)
-win.setWindowTitle("E")
+win.setWindowTitle("Terrain Data - " + version)
+
+
 
 win.show()
 
