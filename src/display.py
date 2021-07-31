5fb288fc5f9ebbb83abb3cd3b727d38c777764bf
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-31 11:40:33 -0400

    create window

--- src/display.py
+++ src/display.py
@@ -4,3 +4,18 @@ import pyqtgraph.console
 import numpy as np
 
 from pyqtgraph.dockarea import *
+
+version = "1.0.0"
+win_title = "Terrain Data - {}", format(version)
+# CREATE APP WINDOW
+app = pg.mkQApp("")
+win = QtGui.QMainWindow()
+area = DockArea()
+win.setCentralWidget(area)
+win.resize(1000,500)
+win.setWindowTitle("E")
+
+win.show()
+
+if __name__ == '__main__':
+    pg.exec()
