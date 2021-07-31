c198c3aba62d4e691abb8c43abe966dddde6c040
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-31 12:28:55 -0400

    create modular docking system

--- src/display.py
+++ src/display.py
@@ -2,7 +2,7 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph.console
 import numpy as np
-from docks.main import createDocks
+from docks.main import createDock
 from pyqtgraph.dockarea import *
 
 version = "1.0.0"
@@ -14,10 +14,12 @@ win.setCentralWidget(area)
 win.resize(1000,500)
 win.setWindowTitle("Terrain Data - " + version)
 
-createDocks(area)
+createDock(area, "Viewer", 'top', 600, 400, False)
+createDock(area, "Console", 'top', 600, 400, False)
 
 win.showMaximized()
 win.show()
+
 
 if __name__ == '__main__':
     pg.exec()
--- src/docks/main.py
+++ src/docks/main.py
@@ -4,10 +4,9 @@ import pyqtgraph.console
 import numpy as np
 
 from pyqtgraph.dockarea import *
-def createDocks(area):
-        d1 = Dock("Viewer", size=(600,500))    
-        d2 = Dock("Console Dock", size=(300,200), closable=False)
+def createDock(area, dock_name, location , xSize, ySize, isClosable):
+        
+        dock_cus = Dock(dock_name, size=(xSize,ySize), closable=isClosable)
+        area.addDock(dock_cus, location)
 
-        area.addDock(d1, 'top')
-        area.addDock(d2, 'bottom')
 
